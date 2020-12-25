9851321eb1bcdd986e438978ddb59faa512e518a
DanMat <dannymatthew@gmail.com>
2020-12-24 21:15:30 -0500

    Adding tests for our sample cli

--- node-cli-template/bin/__tests__/index.test.js
+++ node-cli-template/bin/__tests__/index.test.js
@@ -1,5 +1,32 @@
-import { VERBIAGE } from '../constants';
+const path = require('path');
+const { exec } = require('child_process');
 
-test('Sample test', async () => {
-	expect(VERBIAGE).toEqual(VERBIAGE);
+const { VERBIAGE } = require('../constants');
+
+function cli(args, cwd) {
+	return new Promise(resolve => {
+		exec(
+			`node ${path.resolve('./bin/cli')} ${args.join(' ')}`,
+			{ cwd },
+			(error, stdout, stderr) => {
+				resolve({
+					code: error && error.code ? error.code : 0,
+					error,
+					stdout,
+					stderr
+				});
+			}
+		);
+	});
+}
+
+test('Check the verbiage when a name is passed', async () => {
+	const name = 'Dan';
+	const { stdout } = await cli(['-n', name], '.');
+	expect(stdout).toEqual(`Hello, ${name}. ${VERBIAGE}\n`);
+});
+
+test('Returns an error code 1 with no arg', async () => {
+	const { code } = await cli([], '.');
+	expect(code).toBe(1);
 });
